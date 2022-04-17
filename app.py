22a5eb22576f05f440978f7777fe555d9870e6f5
vchkoz <vchkoz@mail.ru>
2022-04-17 14:12:28 +0300

    12345

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.image as mpimg
 from dataBase import *
-import cv2 as cv
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main():
-     return render_template('main.html')
-
-
+    return render_template('main.html')
 
 
 @app.route('/add', methods=["POST", "GET"])
@@ -30,27 +28,6 @@ def add():
 
     return render_template('add.html')
 
-
-
-
-
-
-
-
-@app.route('/plot')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
